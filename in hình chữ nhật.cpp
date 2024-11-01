1d83f7f4473cdbb151e44c0fe1bf6d995fdab3c6
G-nad <24520255@gm.uit.edu.vn>
2024-11-01 21:52:24 +0700

    Create in hình chữ nhật.cpp

---
+++ in hình chữ nhật.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int n;
+int main(){
+    cin >> n;
+    cout << n << " " << n << " " << n << " " << n << endl;
+    cout << n << "    " << n << endl;
+    cout << n << "    " << n << endl;
+    cout << n << "    " << n << endl;
+    cout << n << " " << n << " " << n << " " << n << endl;
+    return 0;
+}
